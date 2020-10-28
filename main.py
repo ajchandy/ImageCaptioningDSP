d12a3050ec73f370d6099101890ced111656aa43
Prateeth Nayak <nayak044@umn.edu>
2020-10-28 13:37:47 -0400

    main.py Hoon na"

---
+++ main.py
@@ -0,0 +1,14 @@
+#!python3
+
+"""
+MAIN FUNC
+
+This script integrates all the things for training.
+
+On the Contrary we can also put all these things onto a
+Jupy-Notebook
+
+The work on this starts once we have the individual components ready
+& tested
+
+"""
