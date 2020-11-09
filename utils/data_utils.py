19ada01107aea8ccc0c4f22f13b019b5d9d55776
Akhil Chandy <ajchandy@usf.edu>
2020-11-08 23:39:36 -0500

    Comments for pre processing of data

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,23 +1,53 @@
-from keras import vgg16
-img_ids, caption
+from keras.applications.vgg16 import VGG16
+from keras.preprocessing.image import load_img,img_to_array
+from keras.applications.vgg16 import preprocess_input as preprocess_input
+import numpy as np
+from pickle import dump as dump
 
-img_feat_dict = {}
+model=VGG16(include_top=False,pooling='avg')
 
-PATH_OUT = ""
 
-def get_img_feat(path, model='VGG'):
 
-    for ids in img_ids:
-        img = img_to_array(load_img(f"{path_to_img}/{ids}.jpg"))
-        # size = (224,224,3) => (1, 224, 224, 3)
-        img = np.expand_dims(preprocess_img(img), axis=0)
+PATH_IN="C:/Users/akhil/DSP/Flicker Data/Flickr8k_text/Flickr_8k.trainImages.txt"
+PATH_OUT_Inc = "C:/Users/akhil/PycharmProjects/Image-captioning---DSP/pkl files/inception.pkl"
+DATA_PATH = "C:/Users/akhil/DSP/Flicker Data/Flickr8k_Dataset/Flicker8k_Dataset"
 
-        # (1, 1, 1, 2048)
-        feat_vec = vgg16(img)
-        # size (1,1,1,2048) => (1,2048)
-        feat_vec = np.reshape(feat_vec, (1, -1))
-        img_feat_dict[ids] = feat_vec
 
-    dump(img_feat_dict.)
+def get_img_feat(path_in, data_path, path_out, model):
 
-    return PATH_OUT
+    TARGET_SIZE = (224, 224)
+
+    with open(PATH_IN)as f:
+        data = f.read()
+
+    for l in train_img_data.split('\n'):
+        image_id = l.split('.')[0]
+        # Loading individual images
+        img = load_img(f"{DATA_PATH}/{image_id}.jpg", target_size=TARGET_SIZE)
+        # Converting image to array
+        img_array = img_to_array(img)
+        # array shape - (224,224,3)
+        n_img = preprocess_input(img_array)
+        # Adding one more dimension to feed to model in batches
+        n_img = np.expand_dims(n_img, axis=0)
+        # Extracting features from image
+        feat_vec = model.predict(n_img)
+        # feat vec shape (1,512)
+        img_feat_dict[image_id] = np.reshape(feat_vec, (1, -1))
+
+    with open(path_out, 'wb') as f:
+        pickle.dump(object, f)
+"""
+Write code to extract image features as train, test and split blocks
+Also extract captions and make ready to be fed into LSTM
+
+Data cleaning for text convert to lower case, remove special characters 
+Create a corpus with all unique words in the captions
+Step 1 Extract image features
+Step 2 Clean up captions, create corpus
+Step 3 Encode each caption with a start and stop seq token
+Step 4 Map each word in corpus to a higher dimension vecto
+Step 5 Train the LSTM
+  
+"""
+
