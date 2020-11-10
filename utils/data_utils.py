56999a5345d64a593c5f464909431a599fe165b6
Akhil Chandy <ajchandy@usf.edu>
2020-11-09 23:50:18 -0500

    Final code for image feature extraction

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -1,33 +1,36 @@
 from keras.applications.vgg16 import VGG16
 from keras.preprocessing.image import load_img,img_to_array
-from keras.applications.vgg16 import preprocess_input as preprocess_input
+from keras.applications.vgg16 import preprocess_input as preprocess_input_vgg
+from keras.applications.inception_v3 import InceptionV3
+from keras.applications.inception_v3 import preprocess_input as preprocess_input_inc
 import numpy as np
-from pickle import dump as dump
+from pickle import dump,load
 
-model=VGG16(include_top=False,pooling='avg')
-
-
-
-PATH_IN="C:/Users/akhil/DSP/Flicker Data/Flickr8k_text/Flickr_8k.trainImages.txt"
-PATH_OUT_Inc = "C:/Users/akhil/PycharmProjects/Image-captioning---DSP/pkl files/inception.pkl"
+#MODEL = VGG16(include_top=False, pooling='avg')
+MODEL=InceptionV3(include_top=False,pooling='avg')
+PATH_IN = "C:/Users/akhil/DSP/Flicker Data/Flickr8k_text/Flickr_8k.trainImages.txt"
+PATH_OUT_Inc = "C:/Users/akhil/PycharmProjects/Image-captioning---DSP/pkl files/inception_V3.pkl"
 DATA_PATH = "C:/Users/akhil/DSP/Flicker Data/Flickr8k_Dataset/Flicker8k_Dataset"
+PRE_PROCESS = preprocess_input_inc
+
+img_feat_dict = {}
 
 
-def get_img_feat(path_in, data_path, path_out, model):
+def get_img_feat(path_in, data_path, path_out, model, pre_process):
 
     TARGET_SIZE = (224, 224)
 
-    with open(PATH_IN)as f:
-        data = f.read()
+    with open(path_in)as f:
+        train_img_data = f.read()
 
-    for l in train_img_data.split('\n'):
+    for l in train_img_data.split('\n')[:2]:
         image_id = l.split('.')[0]
         # Loading individual images
-        img = load_img(f"{DATA_PATH}/{image_id}.jpg", target_size=TARGET_SIZE)
+        img = load_img(f"{data_path}/{image_id}.jpg", target_size=TARGET_SIZE)
         # Converting image to array
         img_array = img_to_array(img)
         # array shape - (224,224,3)
-        n_img = preprocess_input(img_array)
+        n_img = pre_process(img_array)
         # Adding one more dimension to feed to model in batches
         n_img = np.expand_dims(n_img, axis=0)
         # Extracting features from image
@@ -36,7 +39,7 @@ def get_img_feat(path_in, data_path, path_out, model):
         img_feat_dict[image_id] = np.reshape(feat_vec, (1, -1))
 
     with open(path_out, 'wb') as f:
-        pickle.dump(object, f)
+        dump(img_feat_dict, f)
 """
 Write code to extract image features as train, test and split blocks
 Also extract captions and make ready to be fed into LSTM
