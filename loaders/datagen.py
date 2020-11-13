ab5a7dce2f273cad8d8bec31298b0f3339c97ba8
Prateeth Nayak <nayak044@umn.edu>
2020-11-12 21:00:35 -0500

    #3 Simple tokenized dataloader added

--- loaders/datagen.py
+++ loaders/datagen.py
@@ -10,24 +10,25 @@ from keras.utils.np_utils import to_categorical
 VOCAB_SIZE = 1706
 MAX_LENGTH = 39
 
-def train_loader(img_desc, img_feat,token,bsize):
+
+def train_loader(img_desc, img_feat, token, bsize):
 
     # img_desc = pickle.load(open(desc_path,'r'))
     # img_feat = pickle.load(open(img_feat_path,'r'))
     # token = pickle.load(token_path)
-    inp1, inp2, label = [],[],[]
+    inp1, inp2, label = [], [], []
     cnt = 0
     # while 1:
     for k, desc in img_desc.items():
         cnt += 1
-        ifeat = img_feat[k]
+        ifeat = np.reshape(img_feat[k], (img_feat[k].shape[-1],))
         for d in desc:
             seq = token.texts_to_sequences([d])
             seq = seq[0]
-            for i in range(1,len(seq)):
+            for i in range(1, len(seq)):
                 inpseq, lbl = seq[:i], seq[0]
-                inpseq = pad_sequences([inpseq],maxlen=MAX_LENGTH,padding=post)[0]
-                lbl = to_categorical([lbl],num_classes=VOCAB_SIZE)[0]
+                inpseq = pad_sequences([inpseq], maxlen=MAX_LENGTH, padding="post")[0]
+                lbl = to_categorical([lbl], num_classes=VOCAB_SIZE)[0]
                 inp1.append(ifeat)
                 inp2.append(inpseq)
                 label.append(lbl)
@@ -37,5 +38,14 @@ def train_loader(img_desc, img_feat,token,bsize):
             cnt = 0
 
 
-if __name__=='__main__':
-    #Test the loader
+# Check the Loader
+if __name__ == "__main__":
+    # Test the loader
+    path = "/Users/prateethvnayak/Documents/Agile/Image-captioning---DSP/pkl_files"
+    img_desc = pickle.load(open(f"{path}/train_image_descriptions.pkl", "rb"))
+    img_feat = pickle.load(open(f"{path}/train_image_extracted_VGG16.pkl", "rb"))
+    token = pickle.load(open(f"{path}/token.pkl", "rb"))
+    dl = train_loader(img_desc, img_feat, token, bsize=10)
+    for k in dl:
+        print(k[0][0].shape, k[0][1].shape, k[-1].shape)
+        break
